b8cc30eabca4be75c4d5f4cf045615187a8bb8e9
leokritajessica <leokritajessica@gmail.com>
2020-12-18 14:06:53 +0700

    vowel and consonant word

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 function pigLatin(words) {
-  const vocalWord = ['a', 'i', 'u', 'e', ' o'];
+  const vocalWord = ["a","i","u","e","o"];
   // code here
+  let splitWords = words.split(" ");
 
+  let newWords = splitWords.map((word) => {
+    //jika huruf pertama adalah kata vokal
+    if (vocalWord.includes(word.charAt(0))) {
+      // lanjut ke word selanjutnya
+      return word;
+    } else {
+      // jika konsonan di taruh ke belakang
+      let chars = word.split("");   // hai | apa | kabar => h|a|i
+      let newChars = word.split("");
 
-
+      for (const char of chars) {
+        if (!vocalWord.includes(char)) {
+          let firstChar = newChars.shift();
+          newChars.push(firstChar);
+        } else {
+          break;
+        }
+      }
+      // nambahkan kata ay  pada akhir kata 
+      return newChars.join("") + "ay";
+    }
+  });
+  return newWords.join(" ");
 }
 
-console.log(pigLatin('food')) // ---> oodfay
-console.log(pigLatin('snap')) // ---> apsnay
-console.log(pigLatin('guide')) // ---> uidegay
-console.log(pigLatin('beli makanan')) // ---> elibay akananmay
-console.log(pigLatin('apel')) // ---> apel
+console.log(pigLatin("food")); // ---> oodfay
+console.log(pigLatin("snap")); // ---> apsnay
+console.log(pigLatin("guide")); // ---> uidegay
+console.log(pigLatin("beli makanan")); // ---> elibay akananmay
+console.log(pigLatin("apel")); // ---> apel
