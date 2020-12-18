b2bccc8fa3bbd35d28bdda3fb3d1bbb35648e5c3
nuki-susanti <nuki@Upis-PC.localdomain>
2020-12-18 17:32:56 +0100

    ikan pertama

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-function pigLatin(words) {
-  const vocalWord = ['a', 'i', 'u', 'e', ' o'];
-  // code here
+const vowels = ["a", "i", "o", "u", "e"];
+const ending = "ay";
+function pigLatin(input) {
+  const output = input.toLowerCase().split(" ").map((word) => {
+      if (vowels.includes(word[0])) { //Jika huruf pertama vowels, maka do nothing
+        return input;
+      }
+      let consonantsPrefix = ""; //iterasi setiap element dalam array
+      for (let i = 0; i < word.length; i++) {
+        const char = word[i];
+        if (vowels.includes(char)) {
+          break;
+        }
+        consonantsPrefix += char;
+      }
+      // return (word.substring(consonantsPrefix.length) + consonantsPrefix + ending);
+         return `${word.substring(consonantsPrefix.length)}${consonantsPrefix}${ending}`
+    })
+    .join(" "); //gabungkan lagi array menjadi satu kalimat
+  return output;
+};
 
-
-
-}
-
-console.log(pigLatin('food')) // ---> oodfay
-console.log(pigLatin('snap')) // ---> apsnay
-console.log(pigLatin('guide')) // ---> uidegay
-console.log(pigLatin('beli makanan')) // ---> elibay akananmay
-console.log(pigLatin('apel')) // ---> apel
+console.log(pigLatin("food")) //oodfay
+console.log(pigLatin("Snap")) //apsnay
+console.log(pigLatin("guide")) //uidegay
+console.log(pigLatin("beli makanan")) //elibay akananmay
+console.log(pigLatin("apel")) //apel
